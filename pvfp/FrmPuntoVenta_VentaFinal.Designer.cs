113a576a5ba0b95d463deacd83a5f73fba4515bb
salazar94 <salazar_94@live.com.mx>
2017-03-10 02:13:52 -0700

    Archivos
    no es nuevo, solo para tenerlo

---
+++ pvfp/FrmPuntoVenta_VentaFinal.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace PVFP
+{
+    partial class FrmPuntoVenta_VentaFinal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.BtnCancelar = new System.Windows.Forms.Button();
+            this.Lbl_cambio = new System.Windows.Forms.Label();
+            this.LblCambio = new System.Windows.Forms.Label();
+            this.Btn_Aceptar_pago = new System.Windows.Forms.Button();
+            this.Txt_efectivo = new System.Windows.Forms.TextBox();
+            this.LblPago = new System.Windows.Forms.Label();
+            this.Lbltotal = new System.Windows.Forms.Label();
+            this.Lbl_txtTotal = new System.Windows.Forms.Label();
+            this.txt_tarjeta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_efectivo = new System.Windows.Forms.Button();
+            this.btn_Tarjeta = new System.Windows.Forms.Button();
+            this.btn_dolar = new System.Windows.Forms.Button();
+            this.btn_combinar = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncerrar
+            // 
+            this.btncerrar.Enabled = false;
+            this.btncerrar.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btncerrar.Location = new System.Drawing.Point(356, 556);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(138, 37);
+            this.btncerrar.TabIndex = 38;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancelar
+            // 
+            this.BtnCancelar.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.BtnCancelar.Location = new System.Drawing.Point(445, 475);
+            this.BtnCancelar.Name = "BtnCancelar";
+            this.BtnCancelar.Size = new System.Drawing.Size(139, 51);
+            this.BtnCancelar.TabIndex = 36;
+            this.BtnCancelar.Text = "Cancelar";
+            this.BtnCancelar.UseVisualStyleBackColor = true;
+            this.BtnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
+            // 
+            // Lbl_cambio
+            // 
+            this.Lbl_cambio.AutoSize = true;
+            this.Lbl_cambio.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_cambio.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Lbl_cambio.Location = new System.Drawing.Point(559, 382);
+            this.Lbl_cambio.Name = "Lbl_cambio";
+            this.Lbl_cambio.Size = new System.Drawing.Size(103, 49);
+            this.Lbl_cambio.TabIndex = 35;
+            this.Lbl_cambio.Text = "$0.00";
+            // 
+            // LblCambio
+            // 
+            this.LblCambio.AutoSize = true;
+            this.LblCambio.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCambio.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.LblCambio.Location = new System.Drawing.Point(407, 382);
+            this.LblCambio.Name = "LblCambio";
+            this.LblCambio.Size = new System.Drawing.Size(160, 49);
+            this.LblCambio.TabIndex = 34;
+            this.LblCambio.Text = "Cambio:";
+            // 
+            // Btn_Aceptar_pago
+            // 
+            this.Btn_Aceptar_pago.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Btn_Aceptar_pago.Location = new System.Drawing.Point(292, 474);
+            this.Btn_Aceptar_pago.Name = "Btn_Aceptar_pago";
+            this.Btn_Aceptar_pago.Size = new System.Drawing.Size(139, 51);
+            this.Btn_Aceptar_pago.TabIndex = 33;
+            this.Btn_Aceptar_pago.Text = "Pagar";
+            this.Btn_Aceptar_pago.UseVisualStyleBackColor = true;
+            // 
+            // Txt_efectivo
+            // 
+            this.Txt_efectivo.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Txt_efectivo.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Italic);
+            this.Txt_efectivo.ForeColor = System.Drawing.Color.Silver;
+            this.Txt_efectivo.Location = new System.Drawing.Point(371, 115);
+            this.Txt_efectivo.Name = "Txt_efectivo";
+            this.Txt_efectivo.Size = new System.Drawing.Size(378, 44);
+            this.Txt_efectivo.TabIndex = 0;
+            this.Txt_efectivo.Text = "$ 0.00";
+            // 
+            // LblPago
+            // 
+            this.LblPago.AutoSize = true;
+            this.LblPago.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Italic);
+            this.LblPago.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.LblPago.Location = new System.Drawing.Point(99, 118);
+            this.LblPago.Name = "LblPago";
+            this.LblPago.Size = new System.Drawing.Size(267, 37);
+            this.LblPago.TabIndex = 31;
+            this.LblPago.Text = "Pago en Efectivo:";
+            // 
+            // Lbltotal
+            // 
+            this.Lbltotal.AutoSize = true;
+            this.Lbltotal.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbltotal.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Lbltotal.Location = new System.Drawing.Point(559, 344);
+            this.Lbltotal.Name = "Lbltotal";
+            this.Lbltotal.Size = new System.Drawing.Size(103, 49);
+            this.Lbltotal.TabIndex = 30;
+            this.Lbltotal.Text = "$0.00";
+            // 
+            // Lbl_txtTotal
+            // 
+            this.Lbl_txtTotal.AutoSize = true;
+            this.Lbl_txtTotal.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_txtTotal.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Lbl_txtTotal.Location = new System.Drawing.Point(451, 344);
+            this.Lbl_txtTotal.Name = "Lbl_txtTotal";
+            this.Lbl_txtTotal.Size = new System.Drawing.Size(116, 49);
+            this.Lbl_txtTotal.TabIndex = 29;
+            this.Lbl_txtTotal.Text = "Total:";
+            // 
+            // txt_tarjeta
+            // 
+            this.txt_tarjeta.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txt_tarjeta.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tarjeta.ForeColor = System.Drawing.Color.Silver;
+            this.txt_tarjeta.Location = new System.Drawing.Point(371, 182);
+            this.txt_tarjeta.Name = "txt_tarjeta";
+            this.txt_tarjeta.Size = new System.Drawing.Size(378, 44);
+            this.txt_tarjeta.TabIndex = 1;
+            this.txt_tarjeta.Text = "$ 0.00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Italic);
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(159, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 37);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Pago Tarjeta:";
+            // 
+            // Btn_efectivo
+            // 
+            this.Btn_efectivo.Location = new System.Drawing.Point(196, 22);
+            this.Btn_efectivo.Name = "Btn_efectivo";
+            this.Btn_efectivo.Size = new System.Drawing.Size(124, 63);
+            this.Btn_efectivo.TabIndex = 40;
+            this.Btn_efectivo.Text = "Efectivo";
+            this.Btn_efectivo.UseVisualStyleBackColor = true;
+            // 
+            // btn_Tarjeta
+            // 
+            this.btn_Tarjeta.Location = new System.Drawing.Point(326, 22);
+            this.btn_Tarjeta.Name = "btn_Tarjeta";
+            this.btn_Tarjeta.Size = new System.Drawing.Size(124, 63);
+            this.btn_Tarjeta.TabIndex = 41;
+            this.btn_Tarjeta.Text = "Tarjeta";
+            this.btn_Tarjeta.UseVisualStyleBackColor = true;
+            // 
+            // btn_dolar
+            // 
+            this.btn_dolar.Location = new System.Drawing.Point(456, 22);
+            this.btn_dolar.Name = "btn_dolar";
+            this.btn_dolar.Size = new System.Drawing.Size(124, 63);
+            this.btn_dolar.TabIndex = 42;
+            this.btn_dolar.Text = "Dolar";
+            this.btn_dolar.UseVisualStyleBackColor = true;
+            // 
+            // btn_combinar
+            // 
+            this.btn_combinar.Location = new System.Drawing.Point(586, 22);
+            this.btn_combinar.Name = "btn_combinar";
+            this.btn_combinar.Size = new System.Drawing.Size(124, 63);
+            this.btn_combinar.TabIndex = 43;
+            this.btn_combinar.Text = "Combinar";
+            this.btn_combinar.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox2.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Silver;
+            this.textBox2.Location = new System.Drawing.Point(371, 247);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(378, 44);
+            this.textBox2.TabIndex = 44;
+            this.textBox2.Text = "$ 0.00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Italic);
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(159, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 37);
+            this.label2.TabIndex = 45;
+            this.label2.Text = "Dolar:";
+            // 
+            // FrmPuntoVenta_VentaFinal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 632);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_combinar);
+            this.Controls.Add(this.btn_dolar);
+            this.Controls.Add(this.btn_Tarjeta);
+            this.Controls.Add(this.Btn_efectivo);
+            this.Controls.Add(this.txt_tarjeta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.BtnCancelar);
+            this.Controls.Add(this.Lbl_cambio);
+            this.Controls.Add(this.LblCambio);
+            this.Controls.Add(this.Btn_Aceptar_pago);
+            this.Controls.Add(this.Txt_efectivo);
+            this.Controls.Add(this.LblPago);
+            this.Controls.Add(this.Lbltotal);
+            this.Controls.Add(this.Lbl_txtTotal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmPuntoVenta_VentaFinal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmPuntoVenta_VentaFinal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Button BtnCancelar;
+        private System.Windows.Forms.Label Lbl_cambio;
+        private System.Windows.Forms.Label LblCambio;
+        private System.Windows.Forms.Button Btn_Aceptar_pago;
+        private System.Windows.Forms.TextBox Txt_efectivo;
+        private System.Windows.Forms.Label LblPago;
+        private System.Windows.Forms.Label Lbltotal;
+        private System.Windows.Forms.Label Lbl_txtTotal;
+        private System.Windows.Forms.TextBox txt_tarjeta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_efectivo;
+        private System.Windows.Forms.Button btn_Tarjeta;
+        private System.Windows.Forms.Button btn_dolar;
+        private System.Windows.Forms.Button btn_combinar;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
